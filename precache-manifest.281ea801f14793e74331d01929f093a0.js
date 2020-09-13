01b98e4d2c89b51fd6455b2bc3acf37558d31ce0
Mistreanu Ionut <ionutmistreanu@gmail.com>
2020-09-13 13:52:47 +0300

    Updates

---
+++ precache-manifest.281ea801f14793e74331d01929f093a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05507a7fd51bc15567f9d539b559286b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e1d3a024b734e3af756e",
+    "url": "/monsters-rolodex/static/css/main.53036054.chunk.css"
+  },
+  {
+    "revision": "c19c47a14711e8ddcb65",
+    "url": "/monsters-rolodex/static/js/2.d26298f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d26298f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d3a024b734e3af756e",
+    "url": "/monsters-rolodex/static/js/main.d43a7388.chunk.js"
+  },
+  {
+    "revision": "ad07da4dd552153a336c",
+    "url": "/monsters-rolodex/static/js/runtime-main.ddfa435b.js"
+  }
+]);
